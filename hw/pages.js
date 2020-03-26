2a2ac8b46e2266618a4a7dbd6475aeb6883967ce
LLYee26088 <26088@naver.com>
2020-03-26 12:30:35 +0900

    과제여기있어요

---
+++ hw/pages.js
@@ -0,0 +1,4 @@
+//homework#3
+var studentCount=51;
+var pageSize=10;
+console.log("페이지 수: "+Math.ceil(studentCount/pageSize));
---
+++ hw/random.js
@@ -0,0 +1,4 @@
+//homework#4
+for(let i=0;i<30;i++){
+    console.log(Math.floor(Math.random()*10+10))
+}
